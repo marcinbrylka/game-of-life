dcb18d97a348141967663ecb51c30f5866d40f97
marcinbrylka <marcin_brylka@wp.pl>
2018-06-03 20:50:47 +0200

    layout and board

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("hello");
+
+    function GameOfLife(boardWidth, boardHeight) {
+        this.width = boardWidth;
+        this.height = boardHeight;
+        this.board = document.getElementById("board");
+        this.cell = this.board.querySelector("div");
+        this.cells = [];
+
+        this.createBoard = function () {
+            this.board.style.width = this.width * 10 + 'px';
+            this.board.style.height = this.height * 10 + 'px';
+
+            var numberOfCells = this.width * this.height;
+            for (var i = 0; i < numberOfCells; i++) {
+                var newCell = document.createElement('div');
+                this.board.appendChild(newCell);
+                this.cells.push(newCell);
+            }
+        }
+    }
+
+    var game = new GameOfLife(110, 56);
+    game.createBoard();
+    console.log(game.cells);
+
 });
